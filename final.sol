fc5f7610d4d9923ec996bf4e09c5b2f2a8b1e665
jschraad <jschraad@uni-bremen.de>
2022-04-22 16:11:10 +0200

    Token and stuff

--- final.sol
+++ final.sol
@@ -2,19 +2,23 @@
 
 pragma solidity ^0.8.13;
 
-import "./openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
-contract QuadraticVoting is IERC20{
+contract QuadraticVoting {
     address owner;
     bool is_open;
     uint initial_budget;
     mapping (address => uint) tokens_of_voters;
-    Proposal[] proposals;
+    mapping (uint => Proposal) proposals;
+    VotingToken private tokenLogic;
+    mapping (address => bool) registeredParticipants; 
+    uint private immutable tokenPrice;
+    uint private maxUsedTokens;
+    uint private totalbudget;
 
     struct Proposal {
         address owner;
         string title;
         string description;
+        uint votes;
         uint budget;
         address executable_proposal_address;
         bool is_approved;
@@ -23,14 +27,22 @@ contract QuadraticVoting is IERC20{
     }
 
 
-    constructor(){
+    constructor(uint tokenPrice_, uint maxUsedTokens_, uint tokenCap){
         owner = msg.sender;
+        tokenPrice = tokenPrice_;
+        maxUsedTokens = maxUsedTokens_;
+        totalbudget = msg.value;
+        tokenLogic = new VotingToken("Mark", "RM", tokenCap);
+        is_open = false;
     }
-
-
 
     modifier onlyOwner {
         require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    modifier participantRegistered {
+        require(registeredParticipants[msg.sender], "Address not registered!");
         _;
     }
 
@@ -56,25 +68,48 @@ contract QuadraticVoting is IERC20{
         //TODO 
         //  Participants must transfer Ether when registering to buy tokens (at least one token)
         //   that will be used for casting their votes.
-        
+
         //TODO buy tokens with msg.value
     }
-    
+    /*
     function addProposal(string memory title, string memory description, uint budget, address executable_proposal_address) external votingOpen returns (uint proposal_id  ){
         require(bytes(title).length > 0, "Title can't be empty");
         require(bytes(description).length > 0, "Title can't be empty");
         //TODO verificar address del contrato existe?
         //TODO require onlyParticipant
-        //proposals.push(Proposal(msg.sender, title, description, budget, executable_proposal_address, false, false, 
+        proposals.push(Proposal(msg.sender, title, description, budget, executable_proposal_address, false, false, 
         // TODO como hacer con el mapping
-        //));
+        ));
         return proposals.length - 1;
     }
-
+    */
     function cancelProposal(uint proposal_id) external votingOpen onlyProposalOwner(proposal_id){
             require(!proposals[proposal_id].is_approved, "Approved proposals can't be cancelled.");
             require(!proposals[proposal_id].is_cancelled, "Proposal has been cancelled already.");
             //TODO return tokens
+    }
+
+    function buyTokens() participantRegistered public payable
+    {
+        require(msg.value>0, "You have to buy at least one token!");
+        require((msg.value % tokenPrice)==0, "Watch the token price, only whole tokens can be bought!");
+        require(msg.value/tokenPrice <= maxUsedTokens, "Not enough tokens available!");
+        tokenLogic.mint(msg.sender, msg.value/tokenPrice);
+        tokens_of_voters[msg.sender] += msg.value/tokenPrice;
+        maxUsedTokens -= msg.value/tokenPrice;
+    }
+
+    function sellTokens(uint nrTokens) participantRegistered public payable
+    {
+        require(tokens_of_voters[msg.sender] >= nrTokens, "You don't own so many tokens!"); 
+        tokenLogic.burn(msg.sender, nrTokens);
+        address payable addr = payable(msg.sender);
+        addr.transfer(nrTokens*tokenPrice);       
+    }
+
+    function getERC20Voting() external view returns(VotingToken)
+    {
+        return tokenLogic;
     }
 
     // TODO ver si se puede hacer de una manera mejor
@@ -86,7 +121,7 @@ contract QuadraticVoting is IERC20{
                 number_of_pending_proposals++;
             }
         }
-        
+
         uint[] memory pending_proposals = new uint[](number_of_pending_proposals);
         uint j = 0;
         for(uint i = 0; i < number_of_pending_proposals; j++){
@@ -108,7 +143,7 @@ contract QuadraticVoting is IERC20{
                 number_of_approved_proposals++;
             }
         }
-        
+
         uint[] memory approved_proposals = new uint[](number_of_approved_proposals);
         uint j = 0;
         for(uint i = 0; i < number_of_approved_proposals; j++){
@@ -130,7 +165,7 @@ contract QuadraticVoting is IERC20{
                 number_of_signaling_proposals++;
             }
         }
-        
+
         uint[] memory signaling_proposals = new uint[](number_of_signaling_proposals);
         uint j = 0;
         for(uint i = 0; i < number_of_signaling_proposals; j++){
@@ -149,6 +184,25 @@ contract QuadraticVoting is IERC20{
         return (proposal.title, proposal.description, proposal.budget);
     }
 
+    function stake(uint proposalId, uint nrVotes) public participantRegistered {
+        require(votingOpen == true, "The voting has not started yet!");        
+        uint nrTokens;
+        if(proposals[proposalId].votes_per_user[msg.sender]>0) {
+            uint currentVotes = proposals[proposalId].votes_per_user[msg.sender];
+            nrTokens = (currentVotes+nrVotes)**2 - currentVotes;
+        } else {
+            nrTokens = nrVotes**2;
+        }
+
+        require(tokens_of_voters[msg.sender]>=nrTokens, "Not enough tokens!");
+        require(tokenLogic.allowance(msg.sender, address(this))>=nrTokens, "Use of tokens needs allowance!");
+        tokenLogic.transferFrom(msg.sender, address(this), nrTokens);
+        tokens_of_voters[msg.sender] -= nrTokens;
+        proposals[proposalId].votes_per_user[msg.sender] += nrVotes;
+        proposals[proposalId].votes += nrVotes;
+        _checkAndExecuteProposal(proposalId);  
+    }
+
     function withdrawFromProposal(uint amount_of_votes, uint proposal_id) external{
         Proposal storage proposal = proposals[proposal_id];
         require(!proposal.is_approved, "The proposal has already been approved");
@@ -156,7 +210,7 @@ contract QuadraticVoting is IERC20{
             //TODO return tokens to participant
     }
 
-    // funciton _checkAndExecuteProposal() internal {
+    // funciton _checkAndcheckAndExecuteProposal() internal {
     //         //Perform checks
     //         //Execute
     //         // TODO etc
@@ -176,78 +230,34 @@ contract QuadraticVoting is IERC20{
         //  be set to a state that allows opening a new voting process.
         // This function might consume a lot of gas, take this into account when programming and testing it.
     }
-    
-       /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value)
 
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-
-    /**
-     * @dev Moves `amount` tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
-    function allowance(address owner, address spender) external view returns (uint256);
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    /**
-     * @dev Moves `amount` tokens from `from` to `to` using the
-     * allowance mechanism. `amount` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
+    function sqrt(uint x) internal pure returns (uint y) 
+    {
+        uint z = (x + 1) / 2;
+         y = x;
+        while (z < y) 
+        {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
 }
 
+contract VotingToken is ERC20
+{
+    uint256 immutable private _cap;
+    constructor (string memory name_, string memory symbol_, uint256 cap) ERC20(name_, symbol_) 
+    {
+        _cap = cap;
+    }
+    
+    function mint(address account, uint256 amount) external  virtual {
+        require(ERC20.totalSupply() + amount <= _cap, "ERC20C: cap exceeded");  //totalSupply of tokens bought already + the new tokens;
+        super._mint(account, amount);
+    }
+    
+    function burn(address sender, uint256 amount) public virtual{
+        super._burn(sender, amount);
+    }
+    
+}
